6905f5a475e9c3e0027bd489ccff04b8f6670a68
mezan <mrmezan0@gmail.com>
2019-12-28 00:04:24 +0600

    data type input function also done

--- dataType.py
+++ dataType.py
@@ -13,3 +13,21 @@ a = [1, 2, 4]
 print(type(a))
 a = (2, 3, 4)
 print(type(a))
+
+# concatenation
+
+name = "Mezan"
+age = 22
+print("I am", name, "and", age, "years old.")
+
+# console input
+# name = input("Enter your name :")
+# age = int(input("Enter your age :"))
+# if age > 18:
+#    print("Welcome", name, "here!")
+# else:
+#    print("Sorry!You are not eligible here.")
+
+# float input
+x = float(input("Enter a temperature:"))
+print(x)
