4d256acbbd007163604e180ba7bdc986b6b2e027
mezan <mrmezan0@gmail.com>
2019-12-27 22:16:54 +0600

    Data types

---
+++ dataType.py
@@ -0,0 +1,15 @@
+a = 34
+print(a)
+print(type(a))
+a = 3.4
+print(type(a))
+a = "Who Am I?"
+print(type(a))
+a = {1, 2, 34, 5}
+print(type(a))
+a = {(1, 2), (3, 1)}
+print(type(a))
+a = [1, 2, 4]
+print(type(a))
+a = (2, 3, 4)
+print(type(a))
