366b9430138e2d576d5bf45656b2b16a3c7bfcf7
mezan <mrmezan0@gmail.com>
2019-12-30 00:47:02 +0600

    Real World Project - 2

---
+++ Real World Project-2.py
@@ -0,0 +1,8 @@
+# Design a program to find avarage of three subject marks
+x1 = int(input("Enter subject-1 marks:"))
+x2 = int(input("Enter subject-2 marks:"))
+x3 = int(input("Enter subject-3 marks:"))
+sum = x1 + x2 + x3
+avg = sum / 3
+print("The average marks:", int(avg))
+print("The average marks:", avg)
