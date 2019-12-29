cde536bd7e679a7e0d5047cdfc5fd3c58cd786dd
mezan <mrmezan0@gmail.com>
2019-12-30 00:40:18 +0600

    Real World Project - 1

---
+++ Real World Project-1.py
@@ -0,0 +1,6 @@
+# you working as software developer.One of your customer is planning to make on 50% discount on the products he is selling so he asked from from you to design a program that give the price of the any product after 50% discount
+
+price = int(input("Enter your product price:"))
+discount = 0.5
+price50P = price - price*discount
+print("Your product price after discount:", price50P)
