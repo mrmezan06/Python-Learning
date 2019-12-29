3c697d690451156ec6c1aba100537f71a5ffc7a5
mezan <mrmezan0@gmail.com>
2019-12-30 01:14:06 +0600

    Real World Project - 3

--- Real World Project-3.py
+++ Real World Project-3.py
@@ -1,5 +1,9 @@
 passed = int(input("Enter the number of passed students:"))
 failed = int(input("Enter the number of failed students:"))
 total = passed + failed
-print("The percentage of passed students :", format((passed/total)*100, ".2f"))
-print("The percentage of failed students :", format((failed/total)*100, ".2f"))
+print("The percentage of passed students :", format((passed/total)*100, ".3f"), "%")
+print("The percentage of failed students :", format((failed/total)*100, ".3f"), "%")
+
+# Auto percentage formatting
+print(format((passed/total), "%"), " of passed students.")
+print(format((failed/total), "%"), " of failed students.")
