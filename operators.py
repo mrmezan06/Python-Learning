af06df3c0256752750cc92cddb8a48c7e14981bf
mezan <mrmezan0@gmail.com>
2019-12-30 00:10:35 +0600

    Operator Part

---
+++ operators.py
@@ -0,0 +1,19 @@
+x = 5
+y = 2
+
+# float division
+print(x/y)
+# int division
+print(x//y)
+# division reminder
+print(x % y)
+# x power of y
+print(x**y)
+# checking equals
+print(x == y)
+# checking not equals
+print(x != y)
+
+x = 5.0
+# store float value bt still int division
+print(x // y)
