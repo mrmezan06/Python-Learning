d8a3bd0ab30cce7452769f923fc83c33b11e13f0
mezan <mrmezan0@gmail.com>
2020-01-10 11:31:38 +0600

    Set Example

---
+++ set.py
@@ -0,0 +1,21 @@
+set1 = {1, 22, 3, 4, 'Akash', 4.5}
+print(set1)
+
+set2 = {1, 3, 5, 6, 4.5}
+print(set2)
+
+# And
+set3 = set1 & set2
+print(set3)
+
+# Union
+set3 = set1 | set2
+print(set3)
+
+# Minus
+set3 = set1 - set2
+print(set3)
+
+# Intersection
+set3 = set1 ^ set2
+print(set3)
--- tuple.py
+++ tuple.py
@@ -8,6 +8,6 @@ print(num[2:4])
 numList = [1, 2, 3, 5]
 
 print(numList)
-#Item assign in list is possible but tuple is not allowed it even set also not allowed
+# Item assign in list is possible but tuple is not allowed it even set also not allowed
 numList[1] = 5
 print(numList)
