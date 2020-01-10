4443fadd7faba0f06c59dc34822033e5a53eeab0
mezan <mrmezan0@gmail.com>
2020-01-10 11:05:19 +0600

    List example

--- List.py
+++ List.py
@@ -5,3 +5,9 @@ True is also count as 1
 """
 print(myList.count(1))
 print(myList.count(3))
+# Print value
+print(myList[2])
+# Print Sequence
+print(myList[1:4])
+# Print negative index
+print(myList[-2])
